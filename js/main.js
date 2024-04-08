b418757295afe01193c7dd6d8bd7463e3ac65a20
AnushMnacakanyan <mnacakanyananush53@gmail.com>
2024-04-08 10:38:17 +0400

    1.1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+const navBtnImg = document.querySelector('#nav-btn-img')
+
+navBtn.onClick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg";
+    } else {
+        navBtnImg.src = "./img/icons/NAV.svg";
+    }
+}
+
+AOS.init({
+    // once:true
+});
