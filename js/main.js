e623941b61bf2b589432d22da7200c2ec52c6d44
AnushMnacakanyan <mnacakanyananush53@gmail.com>
2024-04-14 21:31:06 +0400

    1.2

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ const nav = document.querySelector('#nav')
 const navBtn = document.querySelector('#nav-btn')
 const navBtnImg = document.querySelector('#nav-btn-img')
 
-navBtn.onClick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/icons/nav-close.svg";
-    } else {
-        navBtnImg.src = "./img/icons/NAV.svg";
-    }
-}
+// navBtn.onClick = () => {
+//     if (nav.classList.toggle('open')) {
+//         navBtnImg.src = "./img/icons/nav-close.svg";
+//     } else {
+//         navBtnImg.src = "./img/icons/NAV.svg";
+//     }
+// }
+
+navBtn.addEventListener("click",()=>{
+    nav.classList.toggle("open");
+});
 
 AOS.init({
     // once:true
